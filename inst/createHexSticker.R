2df2b1ac4d70da52188adcadcf76718ab343a36b
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-01 21:17:15 +0000

    remove old hexStickers

--- inst/createHexSticker.R
+++
@@ -1,7 +0,0 @@
-library(hexSticker)
-
-sticker("inst/figures/mouse.png",
-        s_x = 1, s_y = 0.85, s_width = 0.7,
-        package = "squeacr", p_color = "#1034A4", p_y = 1.5,
-        h_size = 1.5, h_color = "#A41034", h_fill = NA,
-        filename = "man/figures/squeacr.png")
