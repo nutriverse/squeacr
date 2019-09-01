7928fbe66b74fd0bae4545c79a0bf751a2e7f97f
Ernest Guevarra <ernest.guevarra@post.harvard.edu>
2019-09-01 20:30:47 +0000

    update squeac.R

--- R/squeacr.R
+++ R/squeacr.R
@@ -1,10 +1,11 @@
 ################################################################################
 #
-#'
 #' @references
 #' Myatt, Mark et al. 2012. Semi-Quantitative Evaluation of Access and
 #' Coverage (SQUEAC)/Simplified Lot Quality Assurance Sampling Evaluation of
 #' Access and Coverage (SLEAC) Technical Reference. Washington, DC: FHI 360/FANTA.
+#'
+#' @keywords internal
 #'
 #' @docType package
 #' @name squeacr
