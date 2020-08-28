5ce1810f6da27780f8068db6a4dd0b5b17fc5037
Ernest Guevarra <ernest@guevarra.io>
2020-08-28 08:11:27 +0100

    update functions

--- data-raw/processData.R
+++ data-raw/processData.R
@@ -2,6 +2,9 @@
 
 library(openxlsx)
 library(readxl)
+library(magrittr)
+library(dplyr)
+library(tidyr)
 
 ## Read cmam databases - Katilu
 katilu <- read_xls(path = "data-raw/cmam/kainuk.xls", sheet = 1, skip = 2)
@@ -11,4 +14,14 @@ katilu <- read_xls(path = "data-raw/cmam/kainuk.xls", sheet = 1, skip = 2)
 kainuk <- read_xls(path = "data-raw/cmam/kainuk.xls", sheet = 2, skip = 2)
 
 
-usethis::use_data(processData.R, overwrite = TRUE)
+## Read Lokori dataset - MUAC at admission
+muac_admission <- read_xlsx(path = "data-raw/cmam/lokori.xlsx",
+                            sheet = 1,
+                            range = "A1:L47")
+
+muac_admission <- muac_admission %>%
+  pivot_longer(cols = Lotubae:Lokori, names_to = "district", values_to = "count")
+
+names(muac_admission) <- c("muac", "district", "count")
+usethis::use_data(muac_admission, overwrite = TRUE, compress = "xz")
+
