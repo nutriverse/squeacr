82a1437f4acf9de4d8583b9ffe4b996e5c7b3ba2
Ernest Guevarra <ernest@guevarra.io>
2020-09-05 05:55:06 +0100

    update documentation

--- R/squeacr.R
+++ R/squeacr.R
@@ -1,5 +1,20 @@
 ################################################################################
 #
+#'
+#' Semi-Quantitative Evaluation of Access and Coverage (SQUEAC) Tools in R
+#'
+#' In the recent past, measurement of coverage has been mainly through
+#' two-stage cluster sampled surveys either as part of a nutrition assessment
+#' or through a specific coverage survey known as Centric Systematic Area
+#' Sampling (CSAS). However, such methods are resource intensive and often only
+#' used for final programme evaluation meaning results arrive too late for
+#' programme adaptation. SQUEAC, which stands for Semi-Quantitative Evaluation
+#' of Access and Coverage, is a low resource method designed specifically to
+#' address this limitation and is used regularly for monitoring, planning and
+#' importantly, timely improvement to programme quality, both for agency and
+#' Ministry of Health (MoH) led programmes. This package provides functions
+#' for use in conducting a SQUEAC investigation.
+#'
 #' @references
 #' Myatt, Mark et al. 2012. Semi-Quantitative Evaluation of Access and
 #' Coverage (SQUEAC)/Simplified Lot Quality Assurance Sampling Evaluation of
