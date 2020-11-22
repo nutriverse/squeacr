06e5153ac99b0b823f92072cb231266665bb866f
Ernest Guevarra <ernest@guevarra.io>
2020-11-22 23:04:50 +0000

    update datasets

--- data-raw/otpData.R
+++ data-raw/otpData.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 otp_beneficiaries <- readxl::read_xlsx(path = "data-raw/cmam/otpEpisode_calculations.xlsx",
                                        sheet = 1,
-                                       range = "A1:M290") %>%
+                                       range = "A1:M330") %>%
   dplyr::relocate(health_facility, .before = age) %>%
   dplyr::relocate(locality, .before = health_facility) %>%
   dplyr::relocate(state, .before = state)
